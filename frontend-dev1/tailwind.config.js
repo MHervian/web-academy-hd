b08420e38b7778c521437dfc7c9d5fe260495f4c
Hervian <bayu.candra@cnaindo.com>
2026-01-08 08:43:17 +0700

    Update tailwind.config untuk safelists

---
+++ frontend-dev1/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./app/Views/**/*.php", "./public/**/*.html", "./public/**/*.js"],
+  safelist: [
+    "min-h-[calc(100vh-80px)]",
+    "bg-[rgba(191,36,65,0.4)]",
+    "hover:bg-[rgba(191,36,65,0.75)]",
+    "[text-shadow:0_2px_8px_rgba(0,0,0,0.7)]",
+    "[text-shadow:0_1px_6px_rgba(0,0,0,0.6)]",
+  ],
+};
